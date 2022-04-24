b33abecc38ae425d8b11a5263af2c97a6a74587f
Olive Nives Nameere <olivenives63@gmail.com>
2022-04-24 16:30:46 +0300

    Added explicit wait to Login page objects, Verified logged in

--- pageObjects/login_page.py
+++ pageObjects/login_page.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-class Login:
+class LoginPage:
     textbox_email_id = "email_section"
     textbox_password_id = "password_section"
     button_login_xpath = '//*[@id="root"]/div/div/div/form/div[4]/button'
@@ -13,18 +15,65 @@ class Login:
         self.driver = driver
 
     def set_email(self, email):
-        self.driver.find_element(By.ID, self.textbox_email_id).clear()
-        self.driver.find_element(By.ID, self.textbox_email_id).send_keys(email)
+        # verify email text element is displayed by explicit wait
+        try:
+            email_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.ID, self.textbox_email_id))
+            )
+            email_element.clear()
+            email_element.send_keys(email)
+
+        finally:
+            pass
 
     def set_password(self, password):
-        self.driver.find_element(By.ID, self.textbox_password_id).clear()
-        self.driver.find_element(By.ID, self.textbox_password_id).send_keys(password)
+        # verify password text element is displayed by explicit wait
+        try:
+            password_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.ID, self.textbox_password_id))
+            )
+            password_element.clear()
+            password_element.send_keys(password)
+
+        finally:
+            pass
 
     def click_login(self):
-        self.driver.find_element(By.XPATH, self.button_login_xpath).click()
+        # verify login btn element is displayed by explicit wait
+        try:
+            login_btn_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, self.button_login_xpath))
+            )
+            login_btn_element.click()
+        finally:
+            pass
 
     def click_account(self):
-        self.driver.find_element(By.XPATH, self.button_account_xpath).click()
+        # verify account btn element is displayed by explicit wait
+        try:
+            button_account_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, self.button_account_xpath))
+            )
+            button_account_element.click()
+        finally:
+            pass
 
     def click_logout(self):
-        self.driver.find_element(By.XPATH, self.link_logout_xpath).click()
+        # verify logout btn element is displayed by explicit wait
+        try:
+            logout_btn_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, self.link_logout_xpath))
+            )
+            logout_btn_element.click()
+        finally:
+            pass
+
+    def return_account_btn(self):
+        # verify account button element is displayed by explicit wait
+        try:
+            account_btn_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, self.button_account_xpath))
+            )
+            return account_btn_element
+        finally:
+            pass
--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,3 +1,41 @@
+import time
 import pytest
 from selenium import webdriver
-from pageObjects.login_page import Login
+from pageObjects.login_page import LoginPage
+
+
+class Test00Login:
+    base_url = "https://nives-dashboard.herokuapp.com/"
+    email = "testing@admin.com"
+    password = "@admin12345"
+
+    def test_homepage_title(self, setup):
+        self.driver = setup
+        self.driver.get(self.base_url)
+        actual_title = self.driver.title
+        if actual_title == "Nives-Admin-Dashboard":
+            assert True
+            self.driver.close()
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_homepage_title.png")
+            self.driver.close()
+            assert False
+
+    def test_login(self, setup):
+        self.driver = setup
+        self.driver.get(self.base_url)
+        self.lp = LoginPage(self.driver)
+        self.lp.set_email(self.email)
+        self.lp.set_password(self.password)
+        self.lp.click_login()
+        account_btn = self.lp.return_account_btn()
+        time.sleep(3)
+
+        if account_btn.is_displayed():
+            assert True
+            self.driver.close()
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
+            self.driver.close()
+            assert False
+
