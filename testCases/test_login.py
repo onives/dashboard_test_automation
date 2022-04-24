04ae6a28c8850b6a5caf3c36b24b4631d4979225
Olive Nives Nameere <olivenives63@gmail.com>
2022-04-24 18:33:02 +0300

    Added custom logger and saving logs into automation.log file

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -3,26 +3,34 @@ import pytest
 from selenium import webdriver
 from pageObjects.login_page import LoginPage
 from utilities.read_properties import ReadConfig
+from utilities.custom_logger import LogGen
 
 
 class Test00Login:
     base_url = ReadConfig.get_application_url()
     email = ReadConfig.get_email()
     password = ReadConfig.get_password()
+    logger = LogGen.loggen()
 
     def test_homepage_title(self, setup):
+
+        self.logger.info("********** Test00Login **********")
+        self.logger.info("********** Verifying Home Page Title **********")
         self.driver = setup
         self.driver.get(self.base_url)
         actual_title = self.driver.title
         if actual_title == "Nives-Admin-Dashboard":
             assert True
             self.driver.close()
+            self.logger.info("********** Home page title test pass **********")
         else:
             self.driver.save_screenshot(".\\Screenshots\\" + "test_homepage_title.png")
             self.driver.close()
+            self.logger.error("********** Home page title test failed **********")
             assert False
 
     def test_login(self, setup):
+        self.logger.info("********** Verifying Login Test **********")
         self.driver = setup
         self.driver.get(self.base_url)
         self.lp = LoginPage(self.driver)
@@ -34,9 +42,11 @@ class Test00Login:
 
         if account_btn.is_displayed():
             assert True
+            self.logger.info("********** Login test passed **********")
             self.driver.close()
         else:
             self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
             self.driver.close()
+            self.logger.error("********** Login test failed **********")
             assert False
 
--- utilities/custom_logger.py
+++ utilities/custom_logger.py
@@ -3,9 +3,10 @@ import logging
 
 class LogGen:
     @staticmethod
-    def logger():
+    def loggen():
         logging.basicConfig(filename=".\\Logs\\automation.log",
-                            format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m%d%Y %I:%M:%S %p')
+                            format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                            force=True)
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         return logger
